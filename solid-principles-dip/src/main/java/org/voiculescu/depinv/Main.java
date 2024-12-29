781b0ec353f70b5c0cebe270f76a7c30232bf2be
Corneliu Voiculescu <corneliu@voiculescu.org>
2024-12-29 18:07:06 +0200

    DIP: Modify by providing the dependencies as parameters

--- solid-principles-dip/src/main/java/org/voiculescu/depinv/Main.java
+++ solid-principles-dip/src/main/java/org/voiculescu/depinv/Main.java
@@ -1,14 +1,22 @@
 package org.voiculescu.depinv;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		
-		Message msg = new Message("This is a message again");
-		MessagePrinter printer = new MessagePrinter();
-		printer.writeMessage(msg, "test_msg.json");
-	}
+    public static void main(String[] args) throws IOException {
+
+        Message msg = new Message("This is a message again");
+        MessagePrinter printer = new MessagePrinter();
+        try (PrintWriter printWriter = new PrintWriter(new FileWriter("test_msg.json"))) {
+            printer.writeMessage(msg, new JSONFormatter(), printWriter);
+        }
+
+        try (PrintWriter printWriter = new PrintWriter(System.out)) {
+            printer.writeMessage(msg, new JSONFormatter(), printWriter);
+        }
+    }
 
 }
